ca5657787adead64072b71e318c4aefd182e3cf4
unknown <>
2017-08-15 16:11:25 +0900

    ADD reflect? vector

---
+++ test0/Assets/ArmBehavior.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArmBehavior : MonoBehaviour {
+	public int speed =1;
+	public Vector3 collisonPoint; 
+	Vector3 axis = new Vector3 (0, 0, -1);
+	// Use this for initialization
+	public GameObject wall;
+	void Start () {
+
+	}
+	 
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKey(KeyCode.UpArrow)==true){
+			 transform.Rotate(Vector3.right*speed);
+		}
+		if(Input.GetKey(KeyCode.DownArrow)==true){
+			transform.Rotate(Vector3.left*speed);
+		}
+		
+	}
+
+    void OnTriggerEnter(Collider col) {
+		/*
+		Vector3 incomingVector=direct;
+		incomingVector = incomingVector.normalized;
+		Vector3 normalVector = col.contacts[0].normal;
+		Vector3 reflectVector = Vector3.Reflect (incomingVector, normalVector);
+		reflectVector = reflectVector.normalized;
+		*/
+		//Debug.Log ("OnTriggerEnter");
+    }
+
+
+
+}
---
+++ test0/Assets/HandBehavior.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HandBehavior : MonoBehaviour {
+	public int speed =1;
+	public Vector3 collisonPoint; 
+	Vector3 axis = new Vector3 (0, 0, -1);
+	// Use this for initialization
+	public GameObject wall;
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	void OnTriggerStay(Collider col){
+		Debug.Log ("child : "+Vector3.Project((collisonPoint-transform.position),axis));
+	}
+	void OnTriggerExit(Collider col){
+		Debug.Log ("object Out!");
+		wall.GetComponent<BoxCollider> ().isTrigger = false;
+	}
+	void OnCollisionEnter(Collision collision) {
+		collisonPoint = collision.contacts [0].point;
+		Debug.Log ("collison point : "+collision.contacts[0].point);
+		Debug.Log ("project vector : "+Vector3.Project(collision.contacts[0].point, axis));
+		wall.GetComponent<BoxCollider> ().isTrigger = true;
+	}
+	void OnCollisionExit(Collision collision){
+
+	}
+	void OnCollisonStay(Collision collision){
+
+	}
+	void OnApplicationQuit(){
+
+	}
+}
